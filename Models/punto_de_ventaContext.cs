b0eb4badd35ba285875c2444f425403bd3835a8d
nigth-dead <coppelzenduo@gmail.com>
2026-05-29 17:27:40 -0600

    conexion corregida

--- Models/punto_de_ventaContext.cs
+++ Models/punto_de_ventaContext.cs
@@ -40,11 +40,6 @@ public partial class punto_de_ventaContext : DbContext
     public virtual DbSet<Usuario> Usuario { get; set; }
 
     public virtual DbSet<Venta> Venta { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySQL("server=localhost;database=punto_de_venta;user=root;password=Honda2054++--");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Categoria>(entity =>
